d56176589cac678abae2f4babb28dd4e3235fc40
rodrigocitadin <citadinrodrigo@gmail.com>
2026-02-10 14:08:02 -0300

    feat: timeout for unreachable nodes

--- internal/broadcast.go
+++ internal/broadcast.go
@@ -1,5 +1,10 @@
 package internal
 
+import (
+	"errors"
+	"time"
+)
+
 type Result[T any] struct {
 	PeerID int
 	Value  T
@@ -11,13 +16,18 @@ func Broadcast[T any](peers []Peer, method string, args any) []Result[T] {
 
 	for _, p := range peers {
 		go func(peer Peer) {
+			done := make(chan error, 1)
 			var reply T
-			err := peer.Call(method, args, &reply)
 
-			resultsChan <- Result[T]{
-				PeerID: peer.ID(),
-				Value:  reply,
-				Err:    err,
+			go func() {
+				done <- peer.Call(method, args, &reply)
+			}()
+
+			select {
+			case err := <-done:
+				resultsChan <- Result[T]{PeerID: peer.ID(), Value: reply, Err: err}
+			case <-time.After(5 * time.Second): // Hard timeout
+				resultsChan <- Result[T]{PeerID: peer.ID(), Err: errors.New("timeout")}
 			}
 		}(p)
 	}
